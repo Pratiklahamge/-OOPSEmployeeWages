9060da439ad638b6fe46575e94c721f996a628ec
pratikl <plahamge@gmail.com>
2021-10-09 16:50:39 +0530

    Employee Present or Absent

---
+++ Employee.java
@@ -0,0 +1,18 @@
+package com.briglabz;
+
+public class Employee {
+	public void EmployeePresentorAbesnt() {
+		int check = (int) (Math.random() * 2);
+		System.out.println("\n\n Check the employee present or absent:");
+
+		if (check == 1)
+			System.out.println("\nemployee is present today");
+		else
+			System.out.println("\nemployee is absent today");
+	}
+	public static void main (String[] args){
+		Employee emp = new Employee();
+		emp.EmployeePresentorAbesnt();
+	}
+}
+	
